06c794d92350833d38014d545f2c5ec3ead93e55
Timur Orudzhov <timur.orudzhov@gmail.com>
2019-05-21 12:01:25 +0300

    delete file name generation

--- postgres-backup-selectel/backup.sh
+++ postgres-backup-selectel/backup.sh
@@ -56,13 +56,13 @@ fi
 
 # env vars needed for aws tools
 export PGPASSWORD=$POSTGRES_PASSWORD
-export FILE_NAME="${POSTGRES_DATABASE}_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz"
+#export FILE_NAME="${POSTGRES_DATABASE}_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz"
 
 POSTGRES_HOST_OPTS="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER $POSTGRES_EXTRA_OPTS"
 
 echo "Creating dump of ${POSTGRES_DATABASE} database from ${POSTGRES_HOST}..."
 
-pg_dump $POSTGRES_HOST_OPTS $POSTGRES_DATABASE | gzip > $FILE_NAME
+pg_dump $POSTGRES_HOST_OPTS $POSTGRES_DATABASE | gzip > dump.sql.gz
 
 export SELECTEL_FILE_UPLOAD_URL="https://api.selcdn.ru/v1/SEL_$SELECTEL_USER_ID/$SELECTEL_CONTAINER_NAME/"
 
@@ -81,6 +81,6 @@ echo "Uploading dump to $SELECTEL_CONTAINER_NAME (${SELECTEL_FILE_UPLOAD_URL})"
 
 # upload to selectel
 echo "Upload command is – curl -i -XPUT \"${SELECTEL_FILE_UPLOAD_URL}\" -H \"X-Auth-Token: $SELECTEL_ACCESS_TOKEN\" -H \"X-Delete-After: $SELECTEL_DELETE_AFTER\" -T $FILE_NAME"
-curl -i -XPUT $SELECTEL_FILE_UPLOAD_URL -H "X-Auth-Token: $SELECTEL_ACCESS_TOKEN" -H "X-Delete-After: $SELECTEL_DELETE_AFTER" -T $FILE_NAME
+curl -i -XPUT $SELECTEL_FILE_UPLOAD_URL -H "X-Auth-Token: $SELECTEL_ACCESS_TOKEN" -H "X-Delete-After: $SELECTEL_DELETE_AFTER" -T dump.sql.gz
 
 echo "SQL backup uploaded successfully"
